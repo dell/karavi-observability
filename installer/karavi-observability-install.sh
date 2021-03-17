e0bc66e4c77f0f99b21a3e1086dd5790b831cdd6
Trevor Dawe <trevor.dawe@dell.com>
2021-03-17 09:01:26 -0300

    Update supported k8s versions (#37)

--- installer/karavi-observability-install.sh
+++ installer/karavi-observability-install.sh
@@ -204,7 +204,7 @@ function verify_karavi_observability() {
     log info "Skipping verification of the environment"
     return
   fi
-  verify_k8s_versions "1.17" "1.19"
+  verify_k8s_versions "1.18" "1.20"
   verify_openshift_versions "4.5" "4.6"
   verify_helm_3
 }
