b500e65f3d622302436c6f06fac576febf7fbc1a
Sharmila Ramamoorthy <sharmila.ramamoorthy@dell.com>
2021-09-28 10:51:15 -0400

    update cert manager crd version

--- installer/karavi-observability-install.sh
+++ installer/karavi-observability-install.sh
@@ -184,7 +184,7 @@ function install_certmanager_crds() {
     log info "Certmanager CRDs are already installed"
   else
     log step "Installing CertManager CRDs" "small"
-    run_command "kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.crds.yaml > ${DEBUGLOG} 2>&1"
+    run_command "kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.crds.yaml > ${DEBUGLOG} 2>&1"
     if [ $? -eq 1 ]; then
       log step_failure
       log error "Unable to create cert-manager CRDs to ${NAMESPACE}."
