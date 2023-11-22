bb5729bf061478981eb22b60704581b3baee5ae5
HarishH-DELL <109663924+HarishH-DELL@users.noreply.github.com>
2023-11-22 10:45:38 +0530

    Observability support for ocp4.14 (#116)

--- installer/karavi-observability-install.sh
+++ installer/karavi-observability-install.sh
@@ -443,7 +443,7 @@ function verify_karavi_observability() {
     return
   fi
   verify_k8s_versions "1.25" "1.28"
-  verify_openshift_versions "4.10" "4.13"
+  verify_openshift_versions "4.11" "4.14"
   verify_helm_3
 }
 
